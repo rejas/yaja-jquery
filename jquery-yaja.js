c09f45b6cec4f5b9d9676c8bf28c3d2dc4ab2515
Veeck <veeck@neofonie.de>
2013-09-06 13:50:28 +0200

    add options, adjust copyright notice

--- jquery-yaja.js
+++ jquery-yaja.js
@@ -1,25 +1,47 @@
-
 /*
- * Yaja - yet another jquery accordion
- * Based on code from Marco van Hylckama Vlieg:
- * http://www.i-marco.nl/weblog/archive/2010/02/27/yup_yet_another_jquery_accordi
+ * Yet Another Jquery Accordion
+ *
+ * Original work Copyright 2007-2010 Marco van Hylckama Vlieg
+ * Modified work Copyright 2013 Michael Veeck
+ *
  */
-(function($) {
+(function($)
+{
     "use strict";
-    $.fn.initMenu = function() {
-        return this.each(function () {
+    $.fn.initMenu = function(options)
+    {
+        var settings =
+        {
+            action : "click"
+        };
+
+        if(options)
+        {
+            if (options.action === 'hover')
+            {
+                options.action = 'mouseenter';
+            }
+            $.extend(settings, options);
+        }
+
+        return this.each(function ()
+        {
             $('.acitem', this).hide();
             $('li.expand > .acitem', this).show();
             $('li.expand > .acitem', this).prev().addClass('active');
-            $('li a', this).click(function (e) {
+            $('li a', this).on (settings.action, function (e)
+            {
                 e.stopImmediatePropagation();
                 var theElement = $(this).next();
                 var parent = this.parentNode.parentNode;
-                if ($(parent).hasClass('noaccordion')) {
-                    if (theElement[0] === undefined) {
+                if ($(parent).hasClass('noaccordion'))
+                {
+                    if (theElement[0] === undefined)
+                    {
                         window.location.href = this.href;
                     }
-                    $(theElement).slideToggle('normal', function () {
+                    $(theElement).slideToggle('normal', function ()
+                    {
                         if ($(this).is(':visible')) {
                             $(this).prev().addClass('active');
                         } else {
@@ -28,20 +50,26 @@
                     });
                     return false;
                 } else {
-                    if (theElement.hasClass('acitem') && theElement.is(':visible')) {
-                        if ($(parent).hasClass('collapsible')) {
-                            $('.acitem:visible', parent).first().slideUp('normal', function () {
+                    if (theElement.hasClass('acitem') && theElement.is(':visible'))
+                    {
+                        if ($(parent).hasClass('collapsible'))
+                        {
+                            $('.acitem:visible', parent).first().slideUp('normal', function ()
+                            {
                                 $(this).prev().removeClass('active');
                             });
                             return false;
                         }
                         return false;
                     }
-                    if (theElement.hasClass('acitem') && !theElement.is(':visible')) {
-                        $('.acitem:visible', parent).first().slideUp('normal', function () {
+                    if (theElement.hasClass('acitem') && !theElement.is(':visible'))
+                    {
+                        $('.acitem:visible', parent).first().slideUp('normal', function ()
+                        {
                             $(this).prev().removeClass('active');
                         });
-                        theElement.slideDown('normal', function () {
+                        theElement.slideDown('normal', function ()
+                        {
                             $(this).prev().addClass('active');
                         });
                         return false;
